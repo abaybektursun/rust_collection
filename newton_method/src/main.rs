a34c7e541430c8d12abeeb3998c10459696cd511
Abay Bektursun <Abay Bektursun>
2017-01-08 22:42:55 -0600

    newton method

---
+++ newton_method/src/main.rs
@@ -0,0 +1,20 @@
+extern crate dataplotlib;
+#[macro_use] extern crate prettytable;
+
+use std::io;
+use std::ops::Div;
+use std::f64;
+use std::str::FromStr;
+use std::env;
+use dataplotlib::util::{linspace, zip2};
+use dataplotlib::plotbuilder::PlotBuilder2D;
+use dataplotlib::plotter::Plotter;
+
+use prettytable::Table;
+use prettytable::row::Row;
+use prettytable::cell::Cell;
+
+fn main() {
+
+    
+}
