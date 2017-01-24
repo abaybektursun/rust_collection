e5fa29813cca6d8cac0879ce85f6cf42e3079e35
Abay Bektursun <abaybektursun@gmail.com>
2017-01-23 20:31:10 -0600

    booiii

---
+++ bad_fibonacci/src/main.rs
@@ -0,0 +1,14 @@
+fn bad_fib(n: u64) -> u64{
+    if n == 0 {
+        return 0;
+    }else if n == 1 {
+        return 1;
+    }else{
+        return bad_fib(n-1) + bad_fib(n-2);
+    }
+}
+//boooooii
+fn main(){
+    let n: u64 = 40; 
+    println!("Fib of {} = {}",n,bad_fib(n));
+}
--- harmonic_oscillator/src/main.rs
+++ harmonic_oscillator/src/main.rs
@@ -1,3 +1,6 @@
+#[macro_use] extern crate conrod;
+extern crate find_folder;
+
 use std::time::Duration;
 use std::thread;
 use std::f32;
@@ -26,6 +29,46 @@ fn x(t: f32, A: f32, w: f32, phi: f32)-> f32{
 }
 
 fn main() {
+    const WIDTH: u32 = 200;
+    const HEIGHT: u32 = 200;
+    
+    use conrod::{widget, Labelable, Positionable, Sizeable, Widget};
+    use conrod::backend::piston::{self, Window, WindowEvents, OpenGL};
+    use conrod::backend::piston::event::UpdateEvent;
+
+    // Change this to OpenGL::V2_1 if not working.
+    let opengl = OpenGL::V3_2;
+    
+    
+    // Construct the window.
+    let mut window: Window = piston::window::WindowSettings::new("Click me!", [WIDTH, HEIGHT])
+        .opengl(opengl).exit_on_esc(true).build().unwrap();
+
+    // Create the event loop.
+    let mut events = WindowEvents::new();
+
+    // construct our `Ui`.
+    let mut ui = conrod::UiBuilder::new([WIDTH as f64, HEIGHT as f64]).build();
+
+    // Generate the widget identifiers.
+    widget_ids!(struct Ids { canvas, counter });
+    let ids = Ids::new(ui.widget_id_generator());
+
+    // Add a `Font` to the `Ui`'s `font::Map` from file.
+    let assets = find_folder::Search::KidsThenParents(3, 5).for_folder("assets").unwrap();
+    let font_path = assets.join("fonts/NotoSans/NotoSans-Regular.ttf");
+    ui.fonts.insert_from_file(font_path).unwrap();
+
+    // Create a texture to use for efficiently caching text on the GPU.
+    let mut text_texture_cache = piston::window::GlyphCache::new(&mut window, WIDTH, HEIGHT);
+
+    // The image map describing each of our widget->image mappings (in our case, none).
+    let image_map = conrod::image::Map::new();
+
+    let mut count = 0;
+
+    
+    
     // Spring constant
     let k: f32 = 100.0;
     // Hanger mass
@@ -52,6 +95,6 @@ fn main() {
         thread::sleep(Duration::from_millis(period_ms as u64));
         t = t + period;
         
-        println!("Position: {}",x(t, A, w, phi));
+        print!("\rPosition: {}",format!("{:.*}", 3, x(t, A, w, phi)));
     }
 }
