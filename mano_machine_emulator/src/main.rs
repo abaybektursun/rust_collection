9ef2975aa05a16293ee3ed845ad7d1e69714b43c
abaybektursun <abaybektursun@gmail.com>
2017-02-18 20:32:40 +0000

    starting Mano Machine

--- mano_machine_emulator/src/main.rs
+++ mano_machine_emulator/src/main.rs
@@ -1,5 +1,16 @@
 
 
 fn main() {
-    
+	const MEM_SIZE:usize = 4096;
+	let mut memory:Vec<i16> = Vec::with_capacity(MEM_SIZE);
+	mem_rand(&mut memory, MEM_SIZE);
+	print!("{}", memory[0]);   
+}
+
+fn mem_rand(memory: &mut Vec<i16>, MEM_SIZE:usize){
+	for x in 0..MEM_SIZE {
+		if x%100 == 0{
+		println!("{}",x);
+		}
+	}	
 }
