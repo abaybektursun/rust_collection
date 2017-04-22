519a1911d74494f4bce893a4d0fcc58af4c2893d
abaybektursun <abaybektursun@gmail.com>
2017-04-22 01:29:05 +0000

    sync

--- ray_tracer/src/main.rs
+++ ray_tracer/src/main.rs
@@ -63,8 +63,9 @@ struct Color_ {             //
 //--------------------------//
 
 trait Source_{
-    fn getLightPosition(&self)
-    fn get Color(&self)
+    fn position(&self) -> Vect_;
+    fn color(&self) -> Color_;
+    fn new_(&self) -> Light_;
 }
 
 
@@ -73,8 +74,10 @@ trait Source_{
 struct Light_{                                          //
     position: Vect_,                                    //
     color: Color_,                                      //
-}impl Light_{                                           //
-    pub fn new() -> Light_ {                            //
+}impl Source_ for Light_{                               //
+    fn position(&self) -> Vect_{return self.position}   //
+    fn color(&self) -> Color_{return self.color}        //
+    fn new_(&self) -> Light_ {                                //
         Light_ {                                        //
             position: Vect_{x:0.0, y:0.0, z:0.0},       //
             color: Color_{r:1.0,g:1.0,b:1.0,special:0.0}//
@@ -119,7 +122,7 @@ struct Vect_ {                                                                  
                       y: &self.y*val,                                                    //
                       z: &self.z*val};                                                   //
     }                                                                                    //
-    pub fn new() -> Vect_ {                                                              //
+    pub fn new_() -> Vect_ {                                                              //
         Vect_ {                                                                          //
             x: 0.0,                                                                      //
             y: 0.0,                                                                      //
@@ -136,7 +139,7 @@ struct Ray_ {                                         //
     direction: Vect_,                                 //
 }impl Ray_ {                                          //
     //fn value(&self) -> &f64 { &self.x }             //
-    pub fn new() -> Ray_ {                            //
+    pub fn new_() -> Ray_ {                            //
         Ray_ {                                        //
             origin:    Vect_ {x: 0.0, y: 0.0, z: 0.0},//
             direction: Vect_ {x: 1.0, y: 0.0, z: 0.0},//
@@ -155,7 +158,7 @@ struct Camera_ {                                  //
 }                                                 //
 impl Camera_ {                                    //
     //fn value(&self) -> &f64 { &self.x }         //
-    pub fn new() -> Camera_ {                     //
+    pub fn new_() -> Camera_ {                     //
         Camera_ {                                 //
             pos:   Vect_ {x: 0.0, y: 0.0, z: 0.0},//
             dir:   Vect_ {x: 0.0, y: 0.0, z: 1.0},//
@@ -168,7 +171,7 @@ impl Camera_ {                                    //
 
 //-----------------------------------------------//
 trait Object_ {                                  //
-    fn color(&self,) -> Color_;                   //
+    fn color(&self,) -> Color_;                  //
     fn findIntersection(&self,ray: Ray_) -> f64; //
     fn getNormalAt(&self, point: Vect_) -> Vect_;//
 }                                                //
@@ -181,24 +184,24 @@ struct Sphere_{                                                         //
     radius: f64,                                                        //
     color:  Color_,                                                     //
 } impl Object_ for Sphere_{                                             //                          
-    fn getNormalAt(&self, point: Vect_) -> Vect_{                       //                                                
-        //Normal points away from the center of a sphere                //                                                       
-        return point.vectAdd(self.center.negative().normalize());       //                                                                  
+    fn getNormalAt(&self, point: Vect_) -> Vect_{                       //                      
+        //Normal points away from the center of a sphere                //                            
+        return point.vectAdd(self.center.negative().normalize());       //                     
     }                                                                   //    
-    fn findIntersection(&self,ray: Ray_) -> f64{                        //                                               
-        let ray_origin = ray.origin;                                    //                                     
-        let ray_direction = ray.direction;                              //                                           
-        let sphere_center = self.center;                                //                                       
+    fn findIntersection(&self,ray: Ray_) -> f64{                        //                         
+        let ray_origin = ray.origin;                                    //             
+        let ray_direction = ray.direction;                              //                   
+        let sphere_center = self.center;                                //                      
                                                                         //       
         let a = 1.0;                                                    //                   
-        let b = (2.0*(ray_origin.x - sphere_center.x)*ray_direction.x) +//                                                                        
-                (2.0*(ray_origin.y - sphere_center.y)*ray_direction.y) +//                                                                        
-                (2.0*(ray_origin.z - sphere_center.z)*ray_direction.z); //                                                                      
-        let c = (ray_origin.x - sphere_center.x).powf(2.0) +            //                                                           
-                (ray_origin.y - sphere_center.y).powf(2.0) +            //                                                           
-                (ray_origin.z - sphere_center.z).powf(2.0) -            //                                                           
-                (self.radius*self.radius);                              //                                         
-        let discriminant = b*b - 4.0*c;                                 //                                     
+        let b = (2.0*(ray_origin.x - sphere_center.x)*ray_direction.x) +//                     
+                (2.0*(ray_origin.y - sphere_center.y)*ray_direction.y) +// 
+                (2.0*(ray_origin.z - sphere_center.z)*ray_direction.z); //                    
+        let c = (ray_origin.x - sphere_center.x).powf(2.0) +            // 
+                (ray_origin.y - sphere_center.y).powf(2.0) +            // 
+                (ray_origin.z - sphere_center.z).powf(2.0) -            //          
+                (self.radius*self.radius);                              //  
+        let discriminant = b*b - 4.0*c;                                 //   
         if discriminant > 0.0 {
             // Ray itersects the shere
             
@@ -335,10 +338,10 @@ fn main() {
     let maroon = Color_{r:0.5,g:0.25,b:0.25,special:0.0};
     let black  = Color_{r:0.0,g:0.0, b:0.0, special:0.0};
 
-    let light_position = Vect_{x:-7.0, y:10.0, z: -10.0};
-    let light_source   = Light_{position: light_position, color: white};
-    let mut light_sources: Vec<Box<Light_>> = Vec::new();
-    light_source.push(light_source);
+    let mut light_position = Vect_{x:-7.0, y:10.0, z: -10.0};
+    let mut light_source   = Light_{position: light_position, color: white};
+    let mut light_sources: Vec<Box<Source_>> = Vec::new();
+    light_sources.push(Box::new(light_source));
 
     //--------------------------------------------------------------------------------
     let _sphere = Sphere_{center:O,radius: 1.0,color: green};
@@ -394,8 +397,9 @@ fn main() {
             let mut cam_ray = Ray_{origin: cam_ray_origin, direction: cam_ray_direction};
             
             let mut intersections = Vec::new();
+            let scene_objects_copy = scene_objects.clone();
             for idx in 0..scene_objects.len(){
-                intersections.push(scene_objects[idx as usize].findIntersection(cam_ray));
+                intersections.push(scene_objects_copy[idx as usize].findIntersection(cam_ray));
             }
             let intersections_copy = intersections.clone(); 
             let index_of_winning_object = winningObjectIndex(intersections_copy);
@@ -439,7 +443,7 @@ fn getColorAt(intersection_position: Vect_,
               intersecting_ray_direction: Vect_, 
               scene_objects: Vec<Box<Object_>>, 
               index_of_winning_object: i32, 
-              light_sources: Vec<Box<Light_>>, 
+              light_sources: Vec<Box<Source_>>, 
               accuracy: f64, ambientlight: f64) -> Color_{
-    
+    return Color_{r:0.0,g:0.0,b:0.0,special: 0.0};
 }
