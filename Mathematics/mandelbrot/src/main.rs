6812b2ef075b817df29385191bf1612efc8c004a
Abay Bektursun <abaybektursun@gmail.com>
2017-07-17 14:12:24 -0500

    madnelbrot

--- Mathematics/mandelbrot/src/main.rs
+++ Mathematics/mandelbrot/src/main.rs
@@ -1,5 +1,9 @@
 extern crate image;
 
+use std::fs::File;
+use std::path::Path;
+use image::GenericImage;
+
 #[derive(Copy, Clone)]
 struct Complex{
     real:    f64,
@@ -10,19 +14,19 @@ impl Complex{
        Complex {
            real:    self.real    + num.real,
            lateral: self.lateral + num.lateral 
-       } 
+       }
     }
-	fn sub(&self, num: &Complex) -> Complex{
+    fn sub(&self, num: &Complex) -> Complex{
        Complex {
            real:    self.real    - num.real,
            lateral: self.lateral - num.lateral
-       } 
+       }
     }
     fn mul(&self, num: &Complex) -> Complex{
-       Complex {                                  
+       Complex {
            real:    self.real * num.real + (-1.0 * self.lateral * num.lateral),
            lateral: self.real * num.lateral + self.lateral * num.real  
-       }                                          
+       }
     }
     fn abs(&self) -> f64 {
         ( (self.real.powi(2) + self.real.powi(2) ) as f64).sqrt()
@@ -31,30 +35,35 @@ impl Complex{
 
 fn main() 
 {
-    let x = Complex {real: 1.0, lateral: 4.0};
-    let y = Complex {real: 5.0, lateral: 1.0};
-    let mut test = x.add(&y);
-    println!("r: {}, i: {}", test.real, test.lateral);
-    
-    test = x.sub(&y);
-    println!("r: {}, i: {}", test.real, test.lateral);
-    
-    test = x.mul(&y);
-    println!("r: {}, i: {}\n", test.real, test.lateral);
-
     let zero = Complex {real: 0.0, lateral: -1.0};
     mandelbrot(zero);
+    
+    // Square image
+    let RESOLUTION = 900;
+    let PLANE: f64 = 2.0;
+    let STEP       = 2.0 * PLANE/(RESOLUTION as f64);
 
+    let mut img = image::ImageBuffer::new(RESOLUTION, RESOLUTION);
+    for (x, y, pix) in img.enumerate_pixels_mut() {
+        let r = PLANE - STEP * (x as f64);
+        let i = PLANE - STEP * (y as f64);
+        let iters = mandelbrot(Complex{real: r, lateral: i});
+        
+        *pix = image::Luma([iters]);
+    }
+    let ref mut img_file = File::create(&Path::new("mandel.png")).unwrap();
+    image::ImageLuma8(img).save(img_file, image::PNG);
 }
 
-fn mandelbrot(num: Complex) -> f64 {
+fn mandelbrot(num: Complex) -> u8 {
     let mut z = num;
-    let mut mult = z;
-    for n in 1..50 {
-        if z.abs() > 2.0 { return -1.0}
-        mult = z.mul(&z);
-        z = mult.add(&z);
-        println!("r: {}, i: {}", z.real, z.lateral);
+    //let mut mult = z;
+    let mut iters: u8 = 0;
+    for n in 1..255 {
+        if z.abs() > 2.0 { break; }
+        //mult = z.mul(&z);
+        z = (z.mul(&z) as Complex).add(&num);
+        iters = n;
     }
-    return z.abs();
+    return iters;
 }
