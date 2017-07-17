e36fd2b89916bf5dacb417d08d920dca43948ff3
Abay Bektursun <abaybektursun@gmail.com>
2017-07-16 19:55:49 -0500

    sync

--- Mathematics/mandelbrot/src/main.rs
+++ Mathematics/mandelbrot/src/main.rs
@@ -24,9 +24,13 @@ impl Complex{
            lateral: self.real * num.lateral + self.lateral * num.real  
        }                                          
     }
+    fn abs(&self) -> f64 {
+        ( (self.real.powi(2) + self.real.powi(2) ) as f64).sqrt()
+    }
 }
 
-fn main() {
+fn main() 
+{
     let x = Complex {real: 1.0, lateral: 4.0};
     let y = Complex {real: 5.0, lateral: 1.0};
     let mut test = x.add(&y);
@@ -36,17 +40,21 @@ fn main() {
     println!("r: {}, i: {}", test.real, test.lateral);
     
     test = x.mul(&y);
-    println!("r: {}, i: {}", test.real, test.lateral);
+    println!("r: {}, i: {}\n", test.real, test.lateral);
+
+    let zero = Complex {real: 0.0, lateral: -1.0};
+    mandelbrot(zero);
+
 }
 
-
-fn mandelbrot(num: &Complex) -> f64 {
+fn mandelbrot(num: Complex) -> f64 {
     let mut z = num;
     let mut mult = z;
     for n in 1..50 {
+        if z.abs() > 2.0 { return -1.0}
         mult = z.mul(&z);
         z = mult.add(&z);
         println!("r: {}, i: {}", z.real, z.lateral);
     }
-    return 0.0
+    return z.abs();
 }
